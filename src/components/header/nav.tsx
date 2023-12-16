ad1f1fff33edc4d01ef1392dcb104ce3fab08553
ryoichi0319 <losegroove31@gmail.com>
2023-12-16 16:32:07 +0900

    2

--- src/components/header/nav.tsx
+++ src/components/header/nav.tsx
@@ -35,7 +35,7 @@ export default function Nav() {
         </li>
         <li>
           <Link href="/access" onClick={closeNav}>
-            Access
+            AccessMap
           </Link>
         </li>
         <li>
